8ff4140b39f294b176f4d925620fd540770aae88
mayur-vs <mayurvs.code@gmail.com>
2026-08-01 17:52:11 +0530

    added sub logic

--- hello.py
+++ hello.py
@@ -2,4 +2,7 @@ print("Hello World")
 first_no = 10
 second_no = 20
 sum = first_no + second_no
+sub = first_no - second_no
+
 print("The sum of", first_no, "and", second_no, "is:", sum)
+print("The difference of", first_no, "and", second_no, "is:", sub)
